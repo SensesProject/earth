a73093a81f908382e7b61eb6796ea4e67534d956
Fidel Thomet <me@fidelthomet.com>
2019-01-29 16:09:15 +0100

    [Update] added EarthHeader

---
+++ src/components/EarthHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="EarthHeader">
+  </header>
+</template>
+
+<script>
+import computeFromStore from '../assets/js/computeFromStore.js'
+export default {
+  name: 'EarthHeader',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...computeFromStore(['year', 'scenario'])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.EarthHeader {
+  position: absolute;
+  height: 64px;
+  background: rgba(7,26,41,0.9);
+  width: 100%;
+  bottom: 0;
+  left: 0;
+}
+</style>
