16c3406fb3dec4e740831b3fbb8a774b93e4fc97
Fidel Thomet <fidelthomet@Fidels-MacBook-Pro.local>
2019-01-29 16:05:41 +0100

    [Update] MVP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <VisEarth :grid="grid"/>
+    <EarthOptions/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VisEarth from './components/VisEarth.vue'
+import EarthOptions from './components/EarthOptions.vue'
+
+import tasRCP26 from './assets/data/diff_annual_mean_tas_all-models-rcp26.nc.json'
+import tasRCP60 from './assets/data/tas-rcp60-2025.json'
+import wsi from './assets/data/wsi.json'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VisEarth,
+    EarthOptions,
+  },
+  data () {
+    return {
+      gridData: 'tasRCP60'
+    }
+  },
+  computed: {
+    grid () {
+      if (this.gridData === 'wsi') return wsi
+      if (this.gridData === 'tasRCP26') return tasRCP26
+      if (this.gridData === 'tasRCP60') return tasRCP60
+      return null
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "/assets/style/base";
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/EarthOptions.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="EarthOptions">
+    <input v-model="year" type="range" min="2005" max="2095" step="5">
+    <select v-model="scenario">
+      <option value="rcp26">rcp26</option>
+      <option value="rcp60">rcp60</option>
+    </select>
+
+  </div>
+</template>
+
+<script>
+import computeFromStore from '../assets/js/computeFromStore.js'
+export default {
+  name: 'EarthOptions',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    // ...mapFields([
+    //   'year',
+    //   'scenario',
+    //   'variable'
+    // ])
+    // year: {
+    //   get () {
+    //     return this.$store.state.year
+    //   },
+    //   set (value) {
+    //     this.$store.dispatch('updateYear', value)
+    //   }
+    // },
+    // scenario: {
+    //   get () {
+    //     return this.$store.state.scenario
+    //   },
+    //   set (value) {
+    //     this.$store.dispatch('updateScenario', value)
+    //   }
+    // }
+    ...computeFromStore(['year', 'scenario'])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.EarthOptions {
+  position: absolute;
+  height: 128px;
+  background: rgba(7,26,41,0.9);
+  width: 100%;
+  bottom: 0;
+  left: 0;
+}
+</style>
